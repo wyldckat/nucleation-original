76015e670915b3de0149bb55effcff717e0edaaa
Bruno Santos <wyldckat@gmail.com>
2011-04-22 01:45:12 +0100

    Changed headers to reflect the current project.

--- installOF_exp.sh
+++ installOF_exp.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
+#-----------------------------------------------------------------------
+#   Nucleation - Nucleation (an unofficial OpenFOAM Installer) lower
+#the work to have foam in your system.
 #
+#   visit: http://code.google.com/p/nucleation/
+#
+#-----------------------------------------------------------------------
+#    This file is part of Nucleation.
+#
+#    Nucleation is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Nucleation is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Nucleation.  If not, see <http://www.gnu.org/licenses/>.
+#-----------------------------------------------------------------------
 #
 # USAGE: $ chmod +x installOF.sh
 #		 $ ./installOF.sh
-#
-# THIS SCRIPT IS UNDER GPLv3 LICENSE
-# See script home at:
-# http://code.google.com/p/nucleation
 #
 # Several people have contributed for this project on http://www.cfd-online.com
 #-----------------------TODOS--------------------------------------
